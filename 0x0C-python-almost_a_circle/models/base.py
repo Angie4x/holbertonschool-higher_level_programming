818262da58e8374d576fcc7a3a44eb590a465fb7
Angie4x <angiel.aldanac@outlook.com>
2022-06-17 11:10:24 -0500

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3                                                              
+"""creating a Base class"""
+import json
+from os import path
+
+
+class Base:
+    """base class of the models"""
+
+    __nb_objects = 0
+    """private class attribute"""
+
+    def __init__(self, id=None):
+        """class constructor"""
+	if id is not None:
+	    self.id = id
+	else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+@staticmethod
+    def to_json_string(list_dictionaries):
+	"""adding the static method that                                        
+         returns the list of the JSON string representation"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+	    return "[]"
+	return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+	""" adding the class method that writes the JSON                        
+        string representation"""
+d = []
+        file = cls.__name__+".json"
+        if list_objs is None:
+            with open(file, "w", encoding="utf-8") as f:
+	        f.write(cls.to_json_string(list_objs))
+
+	for element in list_objs:
+            d.append(element.to_dictionary())
+
+        with open(file, "w", encoding="utf-8")as f:
+            f.write(cls.to_json_string(d))
+@staticmethod
+    def from_json_string(json_string):
+        if not isinstance(json_string, str):
+	    return []
+	if len(json_string) == 0:
+	    return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+         @classmethod
+    def load_from_file(cls):
+        if path.exists(cls.__name__ + ".json") is False:
+	    return []
+        list = []
+	with open(cls.__name__ + ".json", "r", encoding='utf-8') as f:
+            list = cls.from_json_string(f.read())
+	list2 = []
+	for i in list:
+	    if type(i) is dict:
+		list2.append(cls.create(**i))
+            else:
+                list2.append(i)
+        return list2
