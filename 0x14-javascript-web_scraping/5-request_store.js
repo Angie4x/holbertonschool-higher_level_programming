6132ea02028bf34adbc5312c2cef394751d147d5
Angie4x <angiel.aldanac@outlook.com>
2022-08-17 23:26:24 -0500

    ok

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,6 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const dest = process.argv[3];
-
 request(url, function (err, response, body) {
   if (err) throw err;
   fs.writeFile(`${dest}`, body, 'utf-8', function (err) {
