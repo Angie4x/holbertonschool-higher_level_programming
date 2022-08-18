b2ffb0087b77f41e48811273bd48e0e50ea5488e
Angie4x <angiel.aldanac@outlook.com>
2022-08-17 23:11:08 -0500

    0read

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const process = require('process');
-const fs = require('fs');
-fs.readFile(String(process.argv[2]), 'utf8', function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
+
+ const fs = require('fs');
+ const file = process.argv[2];
+ 
+ fs.readFile(file, 'utf-8', (err, data) => {
+   if (err) {
+     console.log(err);
+   } else {
+     console.log(data);
+   }
+ });
