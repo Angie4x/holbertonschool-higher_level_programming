b02166a64bd22ba15af450308e96c4425a0b9304
Angie4x <angiel.aldanac@outlook.com>
2022-08-17 23:19:47 -0500

    ok

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 const url = process.argv[2];
-
 request(url, function (err, response) {
   if (err) console.log(err);
   console.log(`code: ${response.statusCode}`);
