480ce4a9682c062139a17ccbfea0e17608bcfce0
Angie4x <angiel.aldanac@outlook.com>
2022-08-17 22:24:23 -0500

    8esrevers

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    const listReverse = [];
+    for (const i in list) {
+      listReverse.unshift(list[i]);
+    }
+    return listReverse;
+  };
+  
